bcf6379f3b9aafc514142df39d8afe2d59263cb2
Tushar Nain <100490977+tusharnain4578@users.noreply.github.com>
2022-12-31 09:06:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://temp-frontend.onrender.com",
     credentials: true
 }));
 
