769cee4b77da5716f35738f9a349faecf908b7f0
Tushar Nain <100490977+tusharnain4578@users.noreply.github.com>
2022-12-31 07:28:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+
+
+app.post("/", (req, res) => {
+    const name = req.body.name;
+
+    console.log(name);
+
+    res.cookie('name', name, {
+        httpOnly: true
+    });
+
+    res.send("Cookie sent " + name);
+})
+
+
+app.listen(process.env.PORT || 8000, () => console.log("Express server running!"))
